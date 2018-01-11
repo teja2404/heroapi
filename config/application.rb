2068533ba7402bc49f28424b31f47c72d1d38836
teja2404 <arunteja.m@kreatio.com>
2018-01-11 11:49:57 +0530

    angular_api_added

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Angularapi
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :put,:post,:delete, :options]
+      end
+    end
+  end
+end
